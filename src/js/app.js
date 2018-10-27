ca67a965482a95dbb2d94460b225806696815568
ns-m <9117479@gmail.com>
2018-10-27 22:54:46 +0300

    starting sliders (I'm little sick)

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,51 @@ import jqMin from './jquery-1.11.1.min';
 jqMin();
 import main from './main';
 main();
+
+(function() {
+    var slider = document.querySelector('.range');
+    var button = document.querySelector('.range__button');
+
+    button.onmousedown = function(evt) {
+        var buttonCoords = getCoords(button);
+        var shiftX = evt.pageX - buttonCoords.left;
+        var sliderCoords = getCoords(slider);
+        console.log('Drag started');
+
+        document.onmousemove = function(evt) {
+            var left = evt.pageX - shiftX - sliderCoords.left;
+
+            if (left < 0) {
+                left = 0;
+            }
+            var right = slider.offsetWidth - button.offsetWidth;
+
+            if (left > right) {
+                left = right;
+            }
+
+            button.style.left = left + 'px';
+        }
+
+        document.onmouseup = function() {
+            document.onmousemove = document.onmouseup = null;
+            console.log('Drag eneded');
+        };
+
+        return false;
+    };
+
+    // Disable hthml5 drag and drop
+    button.ondragstart = function() {
+        return false;
+    };
+
+    function getCoords(elem) {
+        var box = elem.getBoundingClientRect();
+
+        return {
+            top: box.top + pageYOffset,
+            left: box.left + pageXOffset
+        };
+    }
+}());
